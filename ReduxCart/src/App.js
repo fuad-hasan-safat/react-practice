f6d23e08860d066703d18f074a9b9c5f2d07dadd
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-08-08 09:46:38 +0600

    backend added

--- ReduxCart/src/App.js
+++ ReduxCart/src/App.js
@@ -1,15 +1,63 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
 
 function App() {
-  const showCart = useSelector(state=> state.ui.cartIsVisible)
+
+  const dispatch = useDispatch();
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending Cart Data'
+      })
+    )
+     const response = await fetch('https://redux-backend-9a30c-default-rtdb.firebaseio.com/cart.json', {
+        method: 'Put',
+        body: JSON.stringify(cart),
+      });
+
+      if(!response.ok){
+        throw new Error('Sending Cart Data Failed');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'sucess',
+        title: 'Sucess...',
+        message: 'Sending Cart Data Sucesfully'
+      })
+    )
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error...',
+        message: 'Sending Cart Data Failed!'
+      })
+    )
+    });
+    }
+
+  }, [cart, dispatch]);
   return (
+    <>
+    {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
     <Layout>
-    {showCart && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
+    </>
+    
   );
 }
 
