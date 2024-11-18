47122aa7f00b44ea6a1376454d448b9c5eeb8fc0
fuad-hasan-safat <fuadsafat16@gmail.com>
2024-11-19 01:20:09 +0600

    finished quiz app

--- Quiz App/components/Question.jsx
+++ Quiz App/components/Question.jsx
@@ -1,58 +1,68 @@
-import React, { useState } from 'react'
-import QuestionTimmer from './QuestionTimmer'
-import Answers from './Answers'
-import QUESTIONS from '../src/questions';
+import { useState } from 'react';
+
+import QuestionTimer from './QuestionTimer.jsx';
+import Answers from './Answers.jsx';
+import QUESTIONS from '../src/questions.js';
 
 export default function Question({
-    key,
-    questionText,
-    answers,
-    onSelect,
-    selectedAnswer,
-    handleSkipAnswer
+  index,
+  onSelectAnswer,
+  onSkipAnswer,
 }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null
+  });
 
-    const [answer, setAnswer] = useState({
-        selectedAnswer: '',
-        isCorrect: null,
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null
     })
 
-    function handleSelectAnswer(answer){
-        setAnswer(({
-            selectedAnswer: answer,
-            isCorrect: null
-        }))
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer
+      })
 
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
 
-        setTimeout(()=>{
-            setAnswer(({
-                selectedAnswer: answer,
-                isCorrect: QUESTIONS[key].answers[0] === answer
-            }))
-        }, 1000)
-    }
+  let answerState = '';
 
-    let answerState = '';
-    if(answer.selectedAnswer){
-        answerState = answer.isCorrect ? 'correct' : 'wrong'
-    }
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer) {
+    answerState = 'answered';
+  }
 
-    return (
-
-        <div id='question'>
-
-            <QuestionTimmer
-                timeout={10 * 1000}
-                onTimeout={handleSkipAnswer} />
-
-            <h2>{questionText}</h2>
-
-            <Answers
-                answers={answers}
-                selectedAnswer={selectedAnswer}
-                answerState={answerState}
-                onSelect={handleSelectAnswer}
-                 />
-        </div>
-    )
+  return (
+    <div id="question">
+      <QuestionTimer
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+        mode={answerState} />
+      <h2>{QUESTIONS[index].text}</h2>
+      <Answers
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+      />
+    </div>
+  );
 }
